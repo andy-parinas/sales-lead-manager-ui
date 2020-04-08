6ac80f3c3ef09ad508757e65715973d4c246bdb4
Andy Parinas <parinasadolfo@gmail.com>
2020-04-08 10:43:47 +1000

    Added Dashboard items

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,11 @@
       <h1 class="subheading grey--text">Dashboard</h1>
       <v-divider></v-divider>
       <v-container class="my-5">
-        <v-row>
+
+        <!-- Start of Statistics Information -->
+        <!-- this needs to be on separate components -->
+
+        <v-row class="mb-5">
           <v-col cols="12" sm="6" md="4">
                 <v-card class="mx-auto" color="green darken-2" dark >
                      <v-card-title>
@@ -57,13 +61,59 @@
                 </v-card>
           </v-col>
         </v-row>
+
+        <!-- End of Statistics Information -->
+
+        <v-divider></v-divider>
+
+        <!-- start of Subfranchises Info. This is for Franchise Admin Only -->
+        <!-- this needs to be on sperate components -->
+        <v-row align="center" justify="start" class="mt-8">
+          <v-col cols="12" sm="5" md="5" lg="5">
+            <v-card class="mx-auto">
+              <v-toolbar color="light-blue" dark>
+                <v-toolbar-title>Sub Franchises</v-toolbar-title>
+              </v-toolbar>
+
+              <v-list two-line subheader>
+                <v-list-item v-for="item in items" :key="item.title" @click="itemClicked" >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      <span class="grey--text text--darken-1" >Franchise Number: </span>
+                      <span class="font-weight-bold" > {{ item.title }} </span>
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="mt-1">
+                      <span class="font-weight-medium">{{item.subtitle}}</span>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <!-- End of Subfranchises Info -->
+
       </v-container>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    data: () => ({
+      items: [
+        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: '12321312', subtitle: 'Gold Coast Franchise' },
+        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: '54353534', subtitle: 'South Brisbane Franchise' },
+        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: '65432432', subtitle: 'Sydney Franchise' },
+      ]
+    }),
+
+    methods: {
+      itemClicked: () => {
+        console.log('Item Clicked');
+      }
+    }
 }
 </script>
 
